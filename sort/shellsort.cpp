37deb0feaabd48f12cee26e6002ffcc865e0efa2
downson <downson.yuen@gmail.com>
2023-10-03 22:47:13 +0800

    Add code - epoll ET read

--- sort/shellsort.cpp
+++ sort/shellsort.cpp
@@ -6,12 +6,9 @@ void shellsort(vector<int>& nums) {
     int len = nums.size();
     for (int gap = len/2; gap > 0; gap /= 2) {
         for (int i = gap; i < len; ++i) {
-            int j;
-            int temp = nums[i];
-            for (int j = i; j >= nums && nums[j - gap] > temp; j-= gap) {
-                nums[j] = nums[j - gap];
+            for (int j = i; j - gap >= 0 && nums[j - gap] > nums[j]; j-= gap) {
+                swap(nums[j - gap], nums[j]);
             }
-            nums[j] = temp;
         }
     }
 }
